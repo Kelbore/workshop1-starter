ab96d5c52799697188d582a71c0c8bfa92faf017
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-04-28 15:12:51 -0400

    modified index.html and loan-calculator.js

--- scripts/loan-calculator.js
+++ scripts/loan-calculator.js
@@ -1,1 +1,29 @@
-console.log('hello world!');
+"use strict";
+
+window.onload = init;
+
+function init() {
+    const calculateBtn = document.getElementById("calculateBtn");
+    calculateBtn.onclick = onCalculateBtnClicked;
+}
+
+    const loanAmount = document.getElementById("loanAmount");
+    const interestRate = document.getElementById("interestRate");
+    const loanTerm = document.getElementById("loanTerm");
+
+    const monthlyPayment = document.getElementById("monthlyPayment");
+    const totalCost = document.getElementById("totalCost");
+
+function onCalculateBtnClicked() {
+    const loan = Number(loanAmount.value);
+    const rate = Number(interestRate.value)/100;
+    const ratePerMonth = rate/12;
+    const year = Number(loanTerm.value);
+    const months = year * 12;
+    const payment = loan * ratePerMonth/1-(1+ratePerMonth)**-months;
+    const total = payment * months;
+
+    monthlyPayment.innerHTML = payment.toFixed(2);
+    totalCost.innerHTML = total.toFixed(2);
+}    
+
